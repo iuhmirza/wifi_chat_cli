44c301366111e5f5f77c4ff56ec26e1929051c89
Ibtisam Mirza <iuhmirza@gmail.com>
2023-07-17 16:03:54 +0100

    Create mDNS client

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 	"log"
 	"os"
 
+	"github.com/hashicorp/mdns"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
 )
+
+var servers []*mdns.ServiceEntry
 
 type textMessage struct {
 	Username string `json:"username"`
@@ -17,6 +20,31 @@ type textMessage struct {
 }
 
 func main() {
+	entriesCh := make(chan *mdns.ServiceEntry, 4)
+	mdns.Lookup("_wifichat._tcp", entriesCh)
+
+	go func() {
+		count := 0
+		for entry := range entriesCh {
+			servers = append(servers, entry)
+			fmt.Println(count, " ", entry.Info)
+			count += 1
+		}
+	}()
+
+	var choice int
+	fmt.Println("Enter number to connect to server")
+	_, err := fmt.Scanf("%d", &choice)
+	close(entriesCh)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if choice < 0 || len(servers) <= choice {
+		log.Println("choice out of range")
+		return
+	}
+
 	var username string
 	fmt.Print("Enter your username: ")
 	fmt.Scanf("%s", &username)
@@ -24,7 +52,10 @@ func main() {
 
 	ctx := context.TODO()
 
-	wsConn, _, err := websocket.Dial(ctx, "ws://localhost:8080/text", nil)
+	wsConn, _, err := websocket.Dial(
+		ctx,
+		fmt.Sprintf("ws://%v:%v/text", servers[choice].Addr, servers[choice].Port),
+		nil)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -38,7 +69,7 @@ func main() {
 			case <-stopReceiving:
 				return
 			default:
-				err := wsjson.Read(context.TODO(), wsConn, &msg)
+				err = wsjson.Read(context.TODO(), wsConn, &msg)
 				if err != nil {
 					log.Println(err)
 					return
@@ -56,7 +87,7 @@ func main() {
 			wsConn.Close(websocket.StatusNormalClosure, "Disconnecting")
 			return
 		}
-		err := wsjson.Write(ctx, wsConn, &textMessage{Username: username, Content: line})
+		err = wsjson.Write(ctx, wsConn, &textMessage{Username: username, Content: line})
 		if err != nil {
 			fmt.Println(err)
 			return
